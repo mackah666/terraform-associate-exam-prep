5b830ffd14a62aacc096cc2381ca839a4117e45b
Michael Ackah <michael.ackah@cdl.co.uk>
2020-09-04 14:14:05 +0100

    Created module

--- iam_role666.tf
+++ iam_role666.tf
@@ -1,22 +1,23 @@
 resource "aws_iam_role" "test_role" {
   name = "test_role_666"
 
-  assume_role_policy = <<EOF
-{
-  "Version": "2012-10-17",
-  "Statement": [
+  assume_role_policy = jsonencode(
+<<EOF
     {
-      "Action": "sts:AssumeRole",
-      "Principal": {
-        "Service": "ec2.amazonaws.com"
-      },
-      "Effect": "Allow",
-      "Sid": ""
+    "Version": "2012-10-17",
+    "Statement": [
+        {
+        "Action": "sts:AssumeRole",
+        "Principal": {
+            "Service": "ec2.amazonaws.com"
+        },
+        "Effect": "Allow",
+        "Sid": ""
+        }
+    ]
     }
-  ]
-}
-
 EOF
+  )
  tags = {
     Name        = "test role 666"
     Environment = "Dev"
