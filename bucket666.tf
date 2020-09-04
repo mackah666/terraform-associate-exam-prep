c1e5022021ba8db3d1c3b1beacde9b1f90cf854b
Michael Ackah <michael.ackah@cdl.co.uk>
2020-09-04 10:30:02 +0100

    Added versioning to the bucket

--- bucket666.tf
+++ bucket666.tf
@@ -1,6 +1,11 @@
 resource "aws_s3_bucket" "b" {
   bucket = "my-666-test-bucket"
   acl    = "private"
+  
+  versioning {
+    enabled = true
+  }
+
   tags = {
     Name        = "test bucket"
     Environment = "Dev"
